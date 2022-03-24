7b5e89f88ecc5b873a56925d20083fdaf92c2cd2
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-03-24 05:09:20 +0300

    feat: создать схему пользователя

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 const { PORT = 3000 } = process.env;
 
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
+
+app.listen(PORT);
