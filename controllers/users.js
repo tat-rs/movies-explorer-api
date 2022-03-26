e483664befd33571b3e4c64fc04b1bd469ac3804
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-03-26 10:30:59 +0300

    feat: реализовать регистрацию пользователя

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,44 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 const {
   SUCCESS_CODE_OK,
+  SUCCESS_CODE_CREATED,
+  ERROR_CODE_DUPLICATE,
+  SOLT_ROUND,
 } = require('../utils/constants');
 
 const {
   BadRequestError,
   NotFoundError,
+  ConflictError,
 } = require('../errors/errors');
 
 const optionsOfData = {
   new: true,
   runValidators: true,
+};
+
+const createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  if (!email || !password || !name) {
+    throw new BadRequestError('Заполните все обязательные поля');
+  }
+
+  bcrypt.hash(password, SOLT_ROUND)
+    .then((hash) => User.create({ email, password: hash, name }))
+    .then(() => res.status(SUCCESS_CODE_CREATED).send({ email, name }))
+    .catch((err) => {
+      if (err.name === 'ValidationError' || err.name === 'CastError') {
+        next(new BadRequestError(`Переданы некорректные данные${err.errors.email ? `: ${err.errors.email.message}` : ''}`));
+      } else if (err.code === ERROR_CODE_DUPLICATE) {
+        next(new ConflictError('Пользователь уже зарегистрирован'));
+      } else {
+        next(err);
+      }
+    });
 };
 
 const getUserMe = (req, res, next) => {
@@ -51,6 +78,7 @@ const uptadeUserProfile = (req, res, next) => {
 };
 
 module.exports = {
+  createUser,
   getUserMe,
   uptadeUserProfile,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const {
   getUserMe,
   uptadeUserProfile,
+  createUser,
 } = require('../controllers/users');
 
 const userRouter = express.Router();
@@ -10,5 +11,7 @@ const userRouter = express.Router();
 userRouter.get('/users/me', getUserMe); // возвращает информацию о пользователе (email и имя)
 
 userRouter.patch('/users/me', uptadeUserProfile); // обновляет информацию о пользователе (email и имя)
+
+userRouter.post('/signup', createUser); // регистрация пользователя
 
 module.exports = userRouter;
