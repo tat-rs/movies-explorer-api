6f499bd8f5fa7cc7ef559deb6a213b5d3d7ce4dd
Tatiana Roslyakova <tat-rs@yandex.ru>
2022-03-29 05:06:31 +0300

    refactor: стандартный текст ошибок

--- middlewares/errorsHandler.js
+++ middlewares/errorsHandler.js
@@ -2,9 +2,8 @@ const { ERROR_CODE_DEFAULT } = require('../utils/constants');
 
 module.exports.errorsHandler = (err, req, res, next) => {
   const statusCode = err.statusCode || ERROR_CODE_DEFAULT;
-  const errMessage = err.message || 'Произошла ошибка на стороне сервера';
 
-  res.status(statusCode).send({ message: errMessage });
+  res.status(statusCode).send({ message: err.message });
 
   next();
 };
